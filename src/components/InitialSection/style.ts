14edf7f2da9b6bf91090e12e9fbf5413772018eb
Giulyano Santos <giulyanofelipesantos@gmail.com>
2023-06-24 22:48:14 -0300

    Finish the initial section

---
+++ src/components/InitialSection/style.ts
@@ -0,0 +1,38 @@
+import 'styled-components'
+import styled from 'styled-components'
+
+export const ContainerStyled = styled.div`
+width: 800px;
+height: auto;
+display: flex;
+
+img {
+  width: 400px;
+  height: auto;
+  border-radius: 10px;
+}
+
+`
+
+export const HeadingText = styled.div`
+padding: 20px;
+
+
+h1 {
+  font-size: 50px;
+}
+
+h2 {
+   
+    font-size: 28px;
+    font-weight: 600;
+    line-height: 36px;
+    word-spacing: 0px;
+}
+
+h1, h2, p {
+  text-shadow: #000 2px 3px 2px;
+}
+
+`
+
